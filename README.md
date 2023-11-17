af23f5c5a40d8d29cdf313b51a0b7d3e8e2b90a6
Filippo <filippo.galli.cr@gmail.com>
2023-11-17 16:20:59 +0100

    [Fix] illecit access  [Error] insertion_sort_v2

--- README.md
+++ README.md
@@ -13,8 +13,11 @@ __Author__: Bolshakova Liubov, Galli Filippo
 The first attempt to resolve this challenge on GPU. The shortest path is to modify a bit the function on the file knn.cpp to run the same function, sequentially, on GPU.
 This result has been achieved and as everyone could deduce the program is slower than run on CPU.
 
-### knn_gpu - in progress
+### knn_gpu / knn_gpu_v2 - in progress
 
 The second attempt is focused on using the parallel power of GPU. The difference between the first trial is that in this case we define block size and grid size and each thread processed a query.
 It works and is faster than knn_gpu_1_Block_Grid.
-The problem to resolve is that after the kernel is launched when cudaMemcpy from device to host, we have the following error "An illegal memory access was encountered". Although there is this error the function always returns "PASSED", a bit strange behaviour.  
+The problem to resolve is that after the kernel is launched when cudaMemcpy from device to host, we have the following error "An illegal memory access was encountered". Although there is this error the function always returns "PASSED", a bit strange behaviour.
+To fix it the query_nb in knn.cpp should be decreased to 128, so I think we are allocating too much space per thread.
+
+Previous of knn_gpu is right, it has too much memory allocated per thread. So I created a function knn_gpu_v2 and insertion_sort_gpu_v2 that work on dist and index in the global memory. The error before doesn't appear but the there is a __problem__ on the Insertion_sort_gpu_v2 that from 3 query it doesn't work well
--- yourSolution.cu
+++ yourSolution.cu
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <cuda.h>
+#include<cuda_runtime.h>
 
 /**
 * Error checking function;
@@ -15,34 +16,72 @@ inline void gpuAssert(cudaError_t code, int line, bool abort = false, bool log =
     }
 }
 
-__device__ void insertion_sort_gpu(float *dist, int *index, int length, int k){
+/**
+* Insertion sort given in knn.cpp
+* It doesn't work on knn_gpu_v2 because we are saving data of ref i not consecutively respect i-1 but at a distance query_nb
+*/
+__device__ void insertion_sort_gpu(float *dist_sort, int *index_sort, int length, int k){
     // Initialise the first index
-    index[0] = 0;
+    index_sort[0] = 0;
 
     // Go through all points
     for (int i=1; i<length; ++i) {
 
         // Store current distance and associated index
-        float curr_dist  = dist[i];
+        float curr_dist  = dist_sort[i];
         int   curr_index = i;
 
         // Skip the current value if its index is >= k and if it's higher the k-th slready sorted mallest value
-        if (i >= k && curr_dist >= dist[k-1]) {
+        if (i >= k && curr_dist >= dist_sort[k-1]) {
             continue;
         }
 
         // Shift values (and indexes) higher that the current distance to the right
         //int j = std::min(i, k-1);
         int j = i < k-1 ? i : k-1; 
-        while (j > 0 && dist[j-1] > curr_dist) {
-            dist[j]  = dist[j-1];
-            index[j] = index[j-1];
+        while (j > 0 && dist_sort[j-1] > curr_dist) {
+            dist_sort[j]  = dist_sort[j-1];
+            index_sort[j] = index_sort[j-1];
             --j;
         }
 
         // Write the current distance and index at their position
-        dist[j]  = curr_dist;
-        index[j] = curr_index; 
+        dist_sort[j]  = curr_dist;
+        index_sort[j] = curr_index; 
+    }
+}
+
+/**
+* Insertion sort given with data coaliscent 
+*/
+__device__ void insertion_sort_gpu_v2(float *dist_sort, int *index_sort, int length, int k, int query_nb, int query_index){
+    // Initialise the first index
+    index_sort[0] = 0;
+
+    // Go through all points
+    for (int i=1; i<length; i++) {
+
+        // Store current distance and associated index
+        float curr_dist  = dist_sort[query_index + i*query_nb];
+        int   curr_index = i;
+
+        // Skip the current value if its index is >= k and if it's higher the k-th slready sorted mallest value    
+        if (i >= k && curr_dist >= dist_sort[query_index + (k-1)*query_nb]) {
+            continue;
+        }
+        
+
+        // Shift values (and indexes) higher that the current distance to the right
+        int j = i < k - 1 ? i : k - 1;
+        while (j > 0 && dist_sort[query_index + (j - 1) * query_nb] > curr_dist) {
+            dist_sort[query_index + j * query_nb] = dist_sort[query_index +  (j - 1) * query_nb];
+            index_sort[query_index +  j * query_nb] = index_sort[(query_index +  j - 1) * query_nb];
+            --j;
+        }
+
+        // Write the current distance and index at their position
+        dist_sort[query_index + j * query_nb] = curr_dist;
+        index_sort[query_index + j * query_nb] = curr_index;
     }
 }
 
@@ -124,21 +163,52 @@ __global__ void knn_gpu(const float *  ref,
             dist[j]  = cosine_distance_gpu(ref, ref_nb, query, query_nb, dim, j, query_index);
             index[j] = j;
         }
-
-        
+     
         // Sort distances / indexes
         insertion_sort_gpu(dist, index, ref_nb, k);
 
         // Copy k smallest distances and their associated index
         for (int j = 0; j < k; ++j) {
-            knn_dist[query_index + j]  = dist[j];
-            knn_index[query_index + j] = index[j];
+            knn_dist[j * query_nb + query_index]  = dist[j];
+            knn_index[j * query_nb + query_index] = index[j];
         }
         
     }
 
     free(index);
     free(dist);
+}
+
+__global__ void knn_gpu_v2(const float *  ref,
+                        int           ref_nb,
+                        const float * query,
+                        int           query_nb,
+                        int           dim,
+                        int           k,
+                        float *       knn_dist,
+                        int *         knn_index,
+                        int *         index, 
+                        float *       dist){
+
+    // Process one query point at the time
+    for (int query_index = blockIdx.x * blockDim.x + threadIdx.x; query_index < query_nb; query_index += blockDim.x * gridDim.x) {
+        
+        // Compute query distance from every reference
+        for(int reference = 0; reference < ref_nb; reference++){
+            index[query_index + reference*query_nb] =  reference;
+
+            dist[query_index + reference*query_nb] = cosine_distance_gpu(ref, ref_nb, query, query_nb, dim, reference, query_index);
+        } 
+
+        //data coalescent insertion sort
+        insertion_sort_gpu_v2(dist, index, ref_nb, k, query_nb, query_index);
+
+        // from coalescent data (index and dist) to sequential data (knn_index and knn_dist)
+        for(int reference = 0; reference < k; ++reference){
+            knn_index[query_index + reference] = index[query_index + reference * query_nb];
+            knn_dist[query_index + reference] = dist[query_index + reference * query_nb];
+        } 
+    }
 }
 
 bool your_solution(const float * ref,
@@ -168,7 +238,7 @@ bool your_solution(const float * ref,
     // ---------------------------------- Variables' declaration ------------------------------- 
 
     int block_size = 1024;
-    int grid = query_nb/block_size;    
+    int grid = (query_nb + block_size -1)/block_size;    
 
     // ---------------------------------- Creating data location on gpu -------------------------------
     // Location for all reference data
@@ -187,6 +257,12 @@ bool your_solution(const float * ref,
     int * knn_index_gpu;
     gpuErrchk(cudaMallocManaged(&knn_index_gpu, query_nb*k*sizeof(int)));
 
+    // Location for index and dist 
+    int * index_gpu;
+    gpuErrchk(cudaMallocManaged(&index_gpu, query_nb*ref_nb*sizeof(int)));
+    float * dist_gpu;
+    gpuErrchk(cudaMallocManaged(&dist_gpu, query_nb*ref_nb*sizeof(float)));
+
     // ---------------------------------- Transfering data on device -------------------------------
 
     gpuErrchk(cudaMemcpy(ref_gpu, ref, ref_nb*dim*sizeof(float), cudaMemcpyHostToDevice));
@@ -196,25 +272,28 @@ bool your_solution(const float * ref,
 
     // ---------------------------------- Kernel launching -------------------------------
 
-    knn_gpu<<<grid, block_size>>>(ref_gpu, ref_nb, query_gpu, query_nb, dim, k, knn_dist_gpu, knn_index_gpu);
+    //knn_gpu<<<grid, block_size>>>(ref_gpu, ref_nb, query_gpu, query_nb, dim, k, knn_dist_gpu, knn_index_gpu);
+    knn_gpu_v2<<<grid, block_size>>>(ref_gpu, ref_nb, query_gpu, query_nb, dim, k, knn_dist_gpu, knn_index_gpu, index_gpu, dist_gpu);
     //knn_gpu_1_Block_Grid<<<1, 1>>>(ref_gpu, ref_nb, query_gpu, query_nb, dim, k, knn_dist_gpu, knn_index_gpu);
 
     // ---------------------------------- Transfering data on host -------------------------------
 
-    cudaDeviceSynchronize();
+    gpuErrchk(cudaDeviceSynchronize());
 
     gpuErrchk(cudaMemcpy(knn_dist, knn_dist_gpu, query_nb*k*sizeof(float), cudaMemcpyDeviceToHost));
     gpuErrchk(cudaMemcpy(knn_index, knn_index_gpu, query_nb*k*sizeof(int), cudaMemcpyDeviceToHost));
 
-    cudaDeviceSynchronize();
-
     // ---------------------------------- Debug section -------------------------------
 
-    /*
-    for(int i = 0; i < k*query_nb; ++i){
-        std::cout << "indexes found: " << knn_index[i] <<" distances " << knn_dist[i] << std::endl;
+    
+    for(int i = 0; i < query_nb; ++i){
+        std::cout<< i <<" query:" << std::endl; 
+
+        for (int j = 0; j < k; ++j){
+            std::cout << "\treference index: " << knn_index[i + j] << " dist: " << knn_dist[i + j] <<std::endl;
+        }
     }
-    */
+    
     
 
     // ---------------------------------- Free memory -------------------------------
@@ -224,5 +303,8 @@ bool your_solution(const float * ref,
     cudaFree(knn_dist_gpu);
     cudaFree(knn_index_gpu);
 
-   return true;
+    cudaFree(index_gpu);
+    cudaFree(dist_gpu);
+    
+    return true;
 }
